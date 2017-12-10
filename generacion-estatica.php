f49091d7d11ae9f91ffdac82e856926ea674828c
sixdialla <sidiazll_92@hotmail.com>
2017-12-10 19:01:18 +0100

    Adición de la cabecera del plugin

---
+++ generacion-estatica.php
@@ -0,0 +1,14 @@
+
+<?php
+/*
+Plugin Name: Generación Estática
+Plugin URI:  https://www.splc2017.net
+Description: Plugin para generar una copia estática de la web.
+Version:     1.0.0
+Author:      Equipo de Generación Estática
+Author URI:  https://1984.lsi.us.es/wiki-egc/index.php/Gesti%C3%B3n_de_generaci%C3%B3n_est%C3%A1tica_del_sitio_web_-_17_18_-_G1#Miembros	
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+*/
+?>
