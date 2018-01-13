1cb1844bf58f00dc772793a4427e724d5def3c4b
Adelay95 <franciscojavierhigueras@gmail.com>
2018-01-13 14:25:03 +0100

    feat: Introducing tests
    Inclusión de tests de prueba y el archivo de configuración .travis.yml para
    comprobar el correcto funcionamiento de la aplicación
    
    Issue #18

--- library/view.php
+++ library/view.php
@@ -10,13 +10,39 @@ function html_generation_content_page_settings(){
 			?>
 			<label>URL del sitio web:&nbsp;</label>
 			<input 	type="text" 
+		 	
 					name="url_value" 
 					id="url_value"
 					placeholder="localhost"
 					value="<?php echo get_option('url_value'); ?>" />
+				<br>
+				<label>Estructura de la web:&nbsp;</label>
+				<select name="structure_value" 
+					id="structure_value">
+				  <option value="" <?php if(get_option('structure_value')=="") echo 'selected'; ?>>Misma estructura que la web (por defecto)</option>
+				  <option value="N1" <?php if(get_option('structure_value')=="N1") echo 'selected'; ?>>HTML en web/, resto de archivos en web/images/</option>
+				  <option value="N4" <?php if(get_option('structure_value')=="N4") echo 'selected'; ?>>HTML en web/, resto de archivos en web/xxx, donde xxx es la extensión del archivo
+				</option>
+				</select>
+				<br>
+				<label>Tipos de archivos:&nbsp;</label>
+				<select name="file_value" 
+					id="file_value">
+				  <option value="p3" <?php if(get_option('file_value')=="p3") echo 'selected'; ?>>Todos los archivos (por defecto)</option>
+				  <option value="p1" <?php if(get_option('file_value')=="p1") echo 'selected'; ?>>Solo archivos HTML</option>
+				  <option value="p2" <?php if(get_option('file_value')=="p2") echo 'selected'; ?>>Solo archivos no HTML</option>
+				</select>
+				<br>
+
+				<label>Añadir páginas de error:&nbsp;</label>
+				<select name="error_value" 
+					id="error_value">
+				  <option value="" <?php if(get_option('error_value')=="") echo 'selected'; ?>>Sí (por defecto)</option>
+				  <option value="o0" <?php if(get_option('error_value')=="o0") echo 'selected'; ?>>No</option>
+	
+				</select>
 			
 			<?php submit_button('Guardar cambios'); ?>
-
 		</form>
 	</div>
 <?php
@@ -28,8 +54,7 @@ if(isset($_REQUEST["generateArchiveButton"])){
 ?>
 <div class="wrap">
 <form action="" method="POST">
-	<button id="generateArchiveButton" name="generateArchiveButton" type="submit" onclick="return confirm('¿Desea continuar?\n\n(Tenga en cuenta que debe haber guardado los cambios si quiere que se utilicen)')" 
-	class="button-primary">Generar la web estática</button>
+	<button id="generateArchiveButton" name="generateArchiveButton" type="submit" onclick="return confirm('¿Desea continuar?\n\n(Tenga en cuenta que debe haber guardado los cambios si quiere que se utilicen)')" class="button-primary">Generar la web estática</button>
 </form>
 </div>
 <?php 
@@ -40,6 +65,12 @@ echo '<br><a href="../wp-content/plugins/generacion-estatica/uploads/static-html
 function html_generation_content_settings(){
 	register_setting('html-generation-content-settings-group',
 					 'url_value');
+	register_setting('html-generation-content-settings-group',
+					 'structure_value');
+	register_setting('html-generation-content-settings-group',
+					 'file_value');
+	register_setting('html-generation-content-settings-group',
+					 'error_value');
 }
 
 ?>
