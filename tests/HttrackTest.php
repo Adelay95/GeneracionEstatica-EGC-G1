72de6b8e5a36691008aada4ac22fa493d05f4373
Adelay95 <franciscojavierhigueras@gmail.com>
2018-01-13 17:15:55 +0100

    fix: Testing
    Issue #19

--- tests/HttrackTest.php
+++ tests/HttrackTest.php
@@ -4,17 +4,8 @@ namespace EGC\GeneracionEstatica\Tests;
 
 class HttrackTest extends AbstractTest
 {
-    public function ejemploGeneracionEstatica(){
+    public function testBaseDeDatos(){
         $url=get_option('url_value');
-	if ($url=="") {
-  	$url="localhost";
-	}
-	$comando1='cd ../wp-content/plugins/generacion-estatica && rm -r * && cd -';
-   	$comando2='cd ../wp-content/plugins/generacion-estatica && mkdir uploads/static-generation  && cd -';
-	$comando3='cd ../wp-content/plugins/generacion-estatica && httrack '.$url.' -O "uploads/static-generation" > uploads/static-generation/res.txt  && cd -';
-	exec($comando1);
-	exec($comando2);
-	exec($comando3);
         $this->assertTrue($url=='http://blog.httrack.com/');
     }
 }
