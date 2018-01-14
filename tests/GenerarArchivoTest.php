5fe2992e519c29fd038cab4db0326eaa3aeadcb2
sixdialla <sidiazll_92@hotmail.com>
2018-01-14 17:06:47 +0100

    test: Se ha modificado el test GenerarArchivo
    Se ha modificado el test de prueba "GenerarArchivo" para que compruebe que
    se genera el zip después de realizar los comandos.
    
    Issue asociada #29

--- tests/GenerarArchivoTest.php
+++ tests/GenerarArchivoTest.php
@@ -10,7 +10,13 @@ class GenerarArchivoTest extends AbstractTest
 	$fileoption="p3";
 	$erroroption="";
 	run_httrack($url,$structureoption,$fileoption,$erroroption);
-	zip_generate($url,$structureoption);	
-    $this->assertTrue($url=='http://blog.httrack.com/');
+	zip_generate($url,$structureoption);
+	$comando1='cd ../uploads && ls';
+	$out1 = array();
+	exec($comando1,$out1);
+    $this->assertTrue($out1[0]=="static-html.zip");
+	echo $out1[0];
     }
 }
+
+
