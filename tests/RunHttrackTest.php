d0ef63cd7b865e8fa1f77664c953a857a4b8dbc9
oskitatirado <oskitatirado@gmail.com>
2018-01-14 21:27:33 +0100

    test: Add new test
    Nuevo test para probar la funcion de run_Httrack
    
    Fix #29

---
+++ tests/RunHttrackTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace EGC\GeneracionEstatica\Tests;
+include(dirname(__FILE__)."/../library/functions.php");
+class RunHttrackTest extends AbstractTest
+{
+    public function testRunHttrack(){
+    $url='http://blog.httrack.com/';
+	$structureoption="N1";
+	$fileoption="p3";
+	$erroroption="";
+	run_httrack($url,$structureoption,$fileoption,$erroroption,"true");
+	$comando1='cd uploads/static-generation && ls && cd -';
+	$out1 = array();
+	exec($comando1,$out1);
+    $this->assertTrue(in_array("web", $out1));
+	$comando2='cd uploads/static-generation/web && ls';
+	$out2 = array();
+	exec($comando2,$out2);
+    $this->assertTrue(in_array("images", $out2));
+	$this->assertTrue(in_array("index.html", $out2));
+    }
+}
